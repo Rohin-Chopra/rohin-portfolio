ec51d9b36bf4811d5b9d7f58954aae11562b28d6
Rohin Chopra <rohinchopra1212@gmail.com>
2023-09-05 00:18:34 +1000

    spacing

--- app/(subpages)/blogs/page.tsx
+++ app/(subpages)/blogs/page.tsx
@@ -5,8 +5,8 @@ const BlogsPage = async () => {
   const posts = await getBlogs();
 
   return (
-    <div className="min-h-screen pt-8 dark:text-white">
-      <div className="container px-8 py-2">
+    <div className="min-h-screen pt-4 dark:text-white md:pt-10">
+      <div className="container px-8">
         <h1 className="mb-4 text-3xl font-bold md:text-4xl">My Blogs</h1>
         {posts.map((post, index) => (
           <div className="my-6 md:my-8 " key={post.slug}>
