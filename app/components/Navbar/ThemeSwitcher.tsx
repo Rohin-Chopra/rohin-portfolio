41444192f6859f111228f28abec1ffdaf9c6f384
Rohin Chopra <rohinchopra1212@gmail.com>
2023-09-04 22:19:54 +1000

    more cleanup

--- app/components/Navbar/ThemeSwitcher.tsx
+++ app/components/Navbar/ThemeSwitcher.tsx
@@ -1,7 +1,5 @@
 import classNames from "classnames";
-import { usePathname } from "next/navigation";
 import type { DetailedHTMLProps, LiHTMLAttributes } from "react";
-import { useRef, useState } from "react";
 import {
   MdComputer,
   MdDarkMode,
@@ -9,7 +7,6 @@ import {
   MdOutlineDarkMode,
 } from "react-icons/md";
 import { twMerge } from "tailwind-merge";
-import { useOnClickOutside } from "../../hooks/useOnClickOutside";
 import { useTheme } from "../../hooks/useTheme";
 import {
   DropdownMenu,
@@ -43,63 +40,49 @@ const ThemeSwitcherSubMenuItem = ({
 };
 
 export const ThemeSwitcher = () => {
-  const ref = useRef<HTMLDivElement>(null);
-  const [isActive, setIsActive] = useState(false);
   const { theme, setTheme } = useTheme();
-  const pathName = usePathname();
-
-  const toggle = () => setIsActive((val) => !val);
-
-  const close = () => setIsActive(false);
-
-  useOnClickOutside(ref, close);
 
   const setThemeToDark = () => {
     setTheme("dark");
-    close();
   };
 
   const setThemeToLight = () => {
     setTheme("light");
-    close();
   };
 
   const setThemeToSystem = () => {
     setTheme("system");
-    close();
   };
 
   return (
-    <div className="relative" ref={ref}>
-      <DropdownMenu>
-        <DropdownMenuTrigger className="align-middle text-3xl md:text-2xl">
-          {theme === "dark" && <MdDarkMode />}
+    <DropdownMenu>
+      <DropdownMenuTrigger className="align-middle text-3xl md:text-2xl">
+        {theme === "dark" && <MdDarkMode />}
 
-          {theme === "light" && <MdLightMode />}
+        {theme === "light" && <MdLightMode />}
 
-          {theme === "system" && <MdOutlineDarkMode />}
-        </DropdownMenuTrigger>
-        <DropdownMenuContent>
-          <DropdownMenuItem
-            className="flex cursor-pointer justify-between px-3 text-lg"
-            onClick={setThemeToDark}
-          >
-            Dark <MdDarkMode />
-          </DropdownMenuItem>
-          <DropdownMenuItem
-            className="flex cursor-pointer justify-between px-3"
-            onClick={setThemeToLight}
-          >
-            Light <MdLightMode />
-          </DropdownMenuItem>
-          <DropdownMenuItem
-            className="flex cursor-pointer justify-between px-3"
-            onClick={setThemeToSystem}
-          >
-            System <MdComputer />
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
-    </div>
+        {theme === "system" && <MdOutlineDarkMode />}
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuItem
+          className="flex cursor-pointer justify-between px-3 "
+          onClick={setThemeToDark}
+        >
+          Dark <MdDarkMode className="text-lg" />
+        </DropdownMenuItem>
+        <DropdownMenuItem
+          className="flex cursor-pointer justify-between px-3"
+          onClick={setThemeToLight}
+        >
+          Light <MdLightMode className="text-lg" />
+        </DropdownMenuItem>
+        <DropdownMenuItem
+          className="flex cursor-pointer justify-between px-3"
+          onClick={setThemeToSystem}
+        >
+          System <MdComputer className="text-lg" />
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 };
