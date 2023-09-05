0d72a90977dee002a65be0248ccb22a5e5aa91eb
Rohin Chopra <rohinchopra1212@gmail.com>
2023-09-05 22:07:46 +1000

    color change

--- app/components/ScrollProgressBar/ScrollProgressBar.tsx
+++ app/components/ScrollProgressBar/ScrollProgressBar.tsx
@@ -25,7 +25,7 @@ export const ScrollProgressBar = () => {
   return (
     <div className="fixed left-0 top-24 z-40 mb-4 h-1 w-full rounded-none bg-slate-100 dark:bg-slate-800">
       <div
-        className="z-50 h-full bg-slate-900 dark:bg-slate-50"
+        className="z-50 h-full bg-dark dark:bg-primary"
         style={{
           width: `${progress}%`,
         }}
