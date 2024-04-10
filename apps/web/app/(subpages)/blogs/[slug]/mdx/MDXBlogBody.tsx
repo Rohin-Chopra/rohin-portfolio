a872477c83ce4141f1c4e034aff0e0c14f2aa524
Rohin Chopra <rohinchopra1212@gmail.com>
2024-04-11 00:43:24 +1000

    ui: support for h5 and h6

--- apps/web/app/(subpages)/blogs/[slug]/mdx/MDXBlogBody.tsx
+++ apps/web/app/(subpages)/blogs/[slug]/mdx/MDXBlogBody.tsx
@@ -15,7 +15,7 @@ type Props = {
 
 export const MDXBlogBody = ({ blog }: Props) => {
   return (
-    <div className="prose dark:prose-invert prose-h2:mb-4 prose-h4:mb-3 prose-p:mb-2 prose-p:text-lg prose-blockquote:border-l-primary prose-code:rounded prose-code:bg-slate-200 prose-code:p-1 prose-li:text-lg dark:text-zinc-200 prose-headings:dark:text-white prose-strong:dark:text-white prose-code:dark:bg-dracula prose-code:dark:text-white lg:max-w-none">
+    <div className="prose dark:prose-invert prose-h2:mb-4 prose-h4:mb-3 prose-h5:font-bold prose-h5:mb-2 prose-h6:font-semibold prose-h6:mb-1 prose-p:mb-2 prose-p:text-lg prose-blockquote:border-l-primary prose-code:rounded prose-code:bg-slate-200 prose-code:p-1 prose-li:text-lg prose-headings:dark:text-white prose-strong:dark:text-white prose-code:dark:bg-dracula prose-code:dark:text-white lg:max-w-none dark:text-zinc-200">
       <MDXRemote
         source={blog.body}
         options={{
